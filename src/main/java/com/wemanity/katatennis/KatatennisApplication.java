b1e40b07a4734a8ddccd5f1c093d34350e4573e9
mouradbbr <mouradbbr@gmail.com>
2021-03-16 00:25:45 +0100

    init project

---
+++ src/main/java/com/wemanity/katatennis/KatatennisApplication.java
@@ -0,0 +1,13 @@
+package com.wemanity.katatennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatatennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatatennisApplication.class, args);
+	}
+
+}
